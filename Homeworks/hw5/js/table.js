44f4299a41c1de33d0e19169daaf0157be2bbe3d
Yash Gangrade <yashgangrade09@gmail.com>
2018-10-01 03:58:34 -0600

    HW5 part 3 in process

--- Homeworks/hw5/js/table.js
+++ Homeworks/hw5/js/table.js
@@ -64,7 +64,6 @@ class Table {
     try{
         //Update Scale Domains
         let that = this;
-        // console.log(this.teamData);
         let dataRangeMax = d3.max(that.teamData, function(d) {
             return (Math.max(d.value[that.goalsMadeHeader], d.value[that.goalsConcededHeader]));
         });
@@ -80,6 +79,10 @@ class Table {
                            .domain([0, dataRangeMax])
                            .range([this.cell.buffer, 2*this.cell.width])
                            .nice();
+
+        this.goalColorScale = d3.scaleLinear()
+                                .domain([0, dataRangeMax])
+                                .range(['#cb181d', '#034e7b']);
         // Create the axes
         
         let goalXAxis = d3.axisTop().scale(this.goalScale);
@@ -96,9 +99,10 @@ class Table {
 
         // this.tableElements = this.teamData;
         // NEVER do a direct assignment, JavaScript works like Pointers in C++ creating a pointer to an original data.
-        // found on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice to use the slice function to copy the array
-        this.tableElements = this.teamData.slice(); 
-        // console.log(this.tableElements);
+        // found on https://stackoverflow.com/questions/9885821/copying-of-an-array-of-objects-to-another-array-without-object-reference-in-java
+        // this.tableElements = this.teamData.slice(); 
+        this.tableElements = JSON.parse(JSON.stringify(this.teamData));
+        console.log(this.tableElements);
 
 
     }
@@ -122,7 +126,133 @@ class Table {
     updateTable() {
         // ******* TODO: PART III *******
 
-        // defining basics 
+        // defining basic variables
+    try{
+
+        let that = this;
+        let deltaText = "Delta Goals";
+        let goalScale = this.goalScale;
+        let cellCenterPoint = this.cell.height/2;
+        console.log(this.tableElements);
+        let maxGames = d3.max(that.tableElements, d => d.value.TotalGames)
+        console.log(maxGames);
+
+        this.gameScale = d3.scaleLinear()
+                           .domain([0, maxGames])
+                           .range([0, this.cell.width - this.cell.buffer]);
+
+        this.aggregateColorScale = d3.scaleLinear()
+                                     .domain([0, maxGames])
+                                     .range(['#feebe2', '#690000']);
+        
+
+        // Starting with the creation of table rows and updating the table and it's elements
+        let tableDisplay = d3.select("#matchTable").select("tbody").selectAll("tr").data(this.tableElements);
+
+        let tableRows = tableDisplay.enter().append("tr");
+        tableDisplay.exit().remove();
+        tableDisplay = tableDisplay.merge(tableRows);
+
+        tableDisplay.attr("id", d => d.key).attr("class", d => d.value.type);
+        console.log(tableDisplay);
+        // mouseout, mouseover, and on click event to be added
+
+        // we have to append the "th" elements for team name now
+        let tableHeadersDisplay = tableDisplay.selectAll("th").data(d =>[d.key]);
+        
+        let tableHeadersDisplayEnter = tableHeadersDisplay.enter()
+                                                          .append("th")
+                                                          .attr("width", this.cell.width)
+                                                          .attr("height", this.cell.height);
+
+        tableHeadersDisplay = tableHeadersDisplay.merge(tableHeadersDisplayEnter);
+        tableHeadersDisplay = tableHeadersDisplay.text(d => d);
+
+        // Append the remaining "td" elements i.e. Goals, Results, Mins, Losses, Total Games
+
+        let tableDataItemsDisplay = tableDisplay.selectAll("td").data(function(d){
+
+                                        let result = [
+                                                        {type: d.value.type, vis: "goals", value:[{type: d.value.type, delta: d.value[deltaText], goals: d.value[that.goalsMadeHeader]}, {type: d.value.type, delta: d.value[deltaText], goals: d.value[that.goalsConcededHeader]}]},
+                                                        {type: d.value.type, vis: "text", value:[d.value.Result.label]},
+                                                        {type: d.value.type, vis: "bar", value:[d.value.Wins]},
+                                                        {type: d.value.type, vis: "bar", value:[d.value.Losses]},
+                                                        {type: d.value.type, vis: "bar", value:[d.value.TotalGames]}
+                                                    ];
+                                        return result;
+                                    });
+
+        let tableDataItemsDisplayEnter = tableDataItemsDisplay.enter().append("td");
+        tableDataItemsDisplay.exit().remove();
+
+        tableDataItemsDisplay = tableDataItemsDisplayEnter.merge(tableDataItemsDisplay);
+
+        // fill out each column now one by one
+        // Starting with the goals column
+
+        let svgGroupGoalID = tableDataItemsDisplay.filter(function(d){return d.vis == "goals";});
+        // console.log(svgGroupGoalID);
+        // let svgGroupGoal = svgGroupGoalID.selectAll("svg").data(d => (d3.select(this).data()));
+        let svgGroupGoal = svgGroupGoalID.selectAll("svg").data(function(d){
+                                        return d3.select(this).data();
+                                      });
+        let svgGroupGoalEnter = svgGroupGoal.enter().append("svg");
+
+        svgGroupGoal = svgGroupGoalEnter.merge(svgGroupGoal);
+        svgGroupGoal.attr("height", this.cell.height).attr("width", this.cell.width*2 + 20);
+
+        // create the rectangles
+        // let svgGoalRect = svgGroupGoal.selectAll("rect").data(d => (d3.select(this).data()));
+        let svgGoalRect = svgGroupGoal.selectAll("rect").data(function(d){ 
+                                        console.log(d3.select(this).data());
+                                        return d3.select(this).data();
+                                      });
+        let svgGoalRectEnter = svgGoalRect.enter().append("rect");
+        svgGoalRect.exit().remove();
+
+        svgGoalRect = svgGoalRectEnter.merge(svgGoalRect);
+        svgGoalRect.classed("goalBar", true);
+
+        svgGoalRect.attr("x", function(d){
+                            let min = null;
+                            if(d.value[0].goals > d.value[1].goals){
+                                min = d.value[1].goals;
+                            }
+                            else{
+                                min = d.value[0].goals;
+                            }
+                            return that.goalScale(min);
+                        })
+                   .attr("y", function(d){
+                        let diff = 5;
+                        if(d.type == "game"){
+                            diff = 3
+                        } 
+                        return (cellCenterPoint - diff);
+                   })
+                   .attr("height", function(d){
+                        if(d.type == "game"){
+                            return that.cell.height*0.25;
+                        }
+                        return that.cell.height*0.5;
+                   })
+                   .attr("width", d => (Math.abs(goalScale(d.value[0].delta))))
+                   .attr("style", function(d){
+                        if(d.value[0].delta > 0){
+                            return "fill: #364e74";
+                        }
+                        else{
+                            return "fill: #be2714"
+                        }
+                   });
+
+
+    }
+    catch(error){
+        console.log(error);    
+    }
+
+
         //Create table rows
 
         //Append th elements for the Team Names
